1ecfc79780831f9a95397a4631aef69e5abc5be4
DaffaPoseidon <ndap.cahyo@gmail.com>
2022-03-12 22:13:04 +0700

    Quiz 1 -Daffa -Nazario -Hafiz

---
+++ Tugas 4/src/App.jsx
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import ReactDOM from 'react-dom'; 
+import React, {Component} from 'react';
+import FormLogin from './formLogin';
+
+function App() {
+  return (
+    <div class="App">
+      <div>
+        <FormLogin/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
