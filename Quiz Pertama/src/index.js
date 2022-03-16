46627611bb3ffa8e881009e96605baa4afaf0b0b
Nazario Safariesqi <nazariotyo@gmail.com>
2022-03-16 21:37:25 +0700

    Implementasi Login dengan JSON-Server

--- Quiz Pertama/src/index.js
+++ Quiz Pertama/src/index.js
@@ -2,13 +2,15 @@ const container = document.querySelector('.blogs');
 const searchForm = document.querySelector('.search');
 
 const renderPosts = async (term) => {
-  let uri = 'http://localhost:3000/posts?_sort=nama&_oder=asc';
+  console.log(term);
+  let uri = 'http://localhost:3000/posts';
   if (term) {
-    uri += `&q=${term}`;
+    uri += `&q=${term}`
   }
 
   const res = await fetch(uri);
   const posts = await res.json();
+  console.log(posts);
 
   let template = '';
   posts.forEach(post => {
@@ -20,13 +22,14 @@ const renderPosts = async (term) => {
         <a href="details.html?id=${post.id}">Read more ... </a>
       </div>
     `
-  })
+  });
+
   container.innerHTML = template;
 }
 
-searchForm.addEventListener('submit', (e) => {
+searchForm.addEventListener('submit', async (e) => {
   e.preventDefault();
   renderPosts(searchForm.term.value.trim());
 })
 
-window.addEventListener('DOMContentLoaded', () => renderPosts());
+window.addEventListener('DOMContentLoaded', async () => renderPosts());
