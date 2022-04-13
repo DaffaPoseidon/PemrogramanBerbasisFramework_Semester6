8be667fdc064fc5d2e7211aa5ad3e9f18873ad12
Nazario Safariesqi <nazariotyo@gmail.com>
2022-04-13 17:33:21 +0700

    Mengimplementasikan login dengan konsep router

--- UTS/src/App.jsx
+++ UTS/src/App.jsx
@@ -1,14 +1,21 @@
 import './App.css';
 import React, {Component} from 'react';
 import FormLogin from './formLogin';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 
 function App() {
   return (
+    <Router>
     <div class="App">
       <div>
-        <FormLogin/>
+        <Routes>
+          <Route path="/">
+            <FormLogin/>
+          </Route>
+        </Routes>
       </div>
     </div>
+    </Router>
   );
 }
 
