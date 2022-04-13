28b733726deb9fa9d79aa49a41712332afa59154
Daffa Poseidon <ndap.cahyo@gmail.com>
2022-04-13 20:14:29 +0700

    HOC

--- UTS/src/App.jsx
+++ UTS/src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css';
 import React, {Component} from 'react';
 import FormLogin from './formLogin';
+import ClickCounter from './components/ClickCounter';
+import HoverCounter from './components/HoverCounter';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 
 function App() {
@@ -11,6 +13,8 @@ function App() {
         <Routes>
           <Route path="/">
             <FormLogin/>
+            <ClickCounter/>
+            <HoverCounter/>
           </Route>
         </Routes>
       </div>
