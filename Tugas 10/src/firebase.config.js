b231b0f4d16c39b4bab9bfe4178eba94f7460d3f
Daffa Poseidon <ndap.cahyo@gmail.com>
2022-05-23 23:12:38 +0700

    Tugas 10

---
+++ Tugas 10/src/firebase.config.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyDgXLg1Q8barASKKHTBSy907cAZu_5paGM",
+    authDomain: "semester6-tugas10-pbf.firebaseapp.com",
+    projectId: "semester6-tugas10-pbf",
+    storageBucket: "semester6-tugas10-pbf.appspot.com",
+    messagingSenderId: "548824135329",
+    appId: "1:548824135329:web:5118570a1f703ba342a2e0",
+    measurementId: "G-7W7VYH78N5"
+};
