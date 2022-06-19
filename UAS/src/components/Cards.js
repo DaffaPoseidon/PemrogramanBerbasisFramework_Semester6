49661529083f628007d9c83c1c3d729a9c7323af
hafizkalam <hafizkalamabdillah@gmail.com>
2022-06-19 11:08:30 +0700

    Create footer

--- UAS/src/components/Cards.js
+++ UAS/src/components/Cards.js
@@ -11,9 +11,7 @@ const Cards = () => {
   const [data, setData] = useState(Cardsdata);
   // console.log(data);
 
-
   const dispatch = useDispatch();
-
 
   const send = (e) => {
     // console.log(e);
@@ -49,61 +47,8 @@ const Cards = () => {
               )
             })
           }
-
         </div>
       </div>
-      {/* <!-- Contact--> */}
-      <section className="contact-section bg-black">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-4 mb-3 mb-md-0">
-              <div className="card py-4 h-100">
-                <div className="card-body text-center">
-                  <i className="fab fa-xbox text-primary mb-2"></i>
-                  <h4 className="text-uppercase m-0">X-BOX</h4>
-                  <hr className="my-4" />
-                  <div className="small text-black-50">
-                    <a href="#!">Beli Sekarang!</a>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="col-md-4 mb-3 mb-md-0">
-              <div className="card py-4 h-100">
-                <div className="card-body text-center">
-                  <i className="fab fa-playstation text-primary mb-2"></i>
-                  <h4 className="text-uppercase m-0">Playstation</h4>
-                  <hr className="my-4" />
-                  <div className="small text-black-50">
-                    <a href="#!">Beli Sekarang!</a>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="col-md-4 mb-3 mb-md-0">
-              <div className="card py-4 h-100">
-                <div className="card-body text-center">
-                  <i className="fas fa-desktop text-primary mb-2"></i>
-                  <h4 className="text-uppercase m-0">PC</h4>
-                  <hr className="my-4" />
-                  <div className="small text-black-50">
-                    <a href="#!">Beli Sekarang!</a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="social d-flex justify-content-center">
-            <a className="mx-2" href="https://twitter.com/NieRGame?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor"><i className="fab fa-twitter"></i></a>
-            <a className="mx-2" href="https://www.facebook.com/NIERGame"><i className="fab fa-facebook-f"></i></a>
-            <a className="mx-2" href="https://github.com/DaffaPoseidon/PemrogramanBerbasisFramework_Semester6/tree/main/UAS"><i className="fab fa-github"></i></a>
-          </div>
-        </div>
-      </section>
-      {/* <!-- Footer--> */}
-      <footer className="footer bg-black small text-center text-white-50">
-        <div className="container">Copyright Daffa, Tio, Hafizh</div>
-      </footer>
     </>
   )
 }
